9792588751808c31eeec6dc28bbd73a83b1b2528
Konganwan <30665559+Konganwan@users.noreply.github.com>
2018-11-06 18:19:53 +0100

    Added main.py and morpion.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  6 17:47:39 2018
+
+@author: oem
+"""
+
+from morpion import Morpion, run
+
+if __name__ == '__main__':
+    mor = Morpion()
+    run(mor)
---
+++ morpion.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+class Jeu
+    __init__()
+    afficher()
+    reinit()
+        - toute case vide (0)
+    joue(case, joueur)
+        verification case valide -> sinon : Erreur (personalisée?)
+        verification case vide -> sinon : Erreur (personalisée?)
+    checkWin()
+        retourne bool
+    grille: liste de 3 liste
+        0: vide
+        1: JA
+        2: JB
+
++ methode run() qui gere quoi est fait quand
+ - nouvelle instance de Jeu
+
+"""
+class Morpion(object):
+    
+    def __init__(self):
+        pass
+    
+    def reinit(self):
+        pass
+    
+    def play(self, case, player):
+        pass
+    
+    def disp(self):
+        pass
+    
+    def check_win(self):
+        pass
+    
+    
+def ren(mor):
+    pass
