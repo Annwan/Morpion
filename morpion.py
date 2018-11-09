b263add89c888ec75fa8abbc584c2f24058b7769
Konganwan <30665559+Konganwan@users.noreply.github.com>
2018-11-09 16:51:07 +0100

    Update morpion.py

--- morpion.py
+++ morpion.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-class Jeu
-    __init__()
-    afficher()
-    reinit()
-        - toute case vide (0)
-    joue(case, joueur)
-        verification case valide -> sinon : Erreur (personalisée?)
-        verification case vide -> sinon : Erreur (personalisée?)
-    checkWin()
-        retourne bool
-    grille: liste de 3 liste
-        0: vide
-        1: JA
-        2: JB
+    Copyright (C) 2018 Konganwan
 
-+ methode run() qui gere quoi est fait quand
- - nouvelle instance de Jeu
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+   
 """
 class Morpion(object):
     
